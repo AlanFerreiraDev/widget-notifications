04d5423d9550f566153ba6241bdc51859e868c7e
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-07-08 14:57:53 -0300

    feat: apply Composition Pattern on Widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,16 @@
-import { Rocket, X, Check } from 'lucide-react'
+'use client'
+
+import { Check, Rocket, X } from 'lucide-react'
+import { Notification } from './Notification'
 
 export function Widget() {
+  const handleClickAccept = () => {
+    console.log('Aceitar')
+  }
+
+  const handleClickIgnore = () => {
+    console.log('Ignorar')
+  }
   return (
     <div className="w-[448px] rounded overflow-hidden">
       {/* Header */}
@@ -21,39 +31,26 @@ export function Widget() {
         </div>
 
         <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-500 dark:text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-          </div>
+          <Notification.Root>
+            <Notification.Icon icon={Rocket} />
+            <Notification.Content text="Você recebeu um convite para fazer parte da empresa Rocketseat." />
+            <Notification.Actions>
+              <Notification.Action onClick={handleClickIgnore} icon={X} />
+              <Notification.Action
+                onClick={handleClickAccept}
+                icon={Check}
+                className="bg-emerald-500 hover:bg-emerald-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
 
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-            <div className="flex gap-2 self-center">
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
-                <X className="w-3 h-3 text-zinc-50" />
-              </button>
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
-                <Check className="w-3 h-3 text-zinc-50" />
-              </button>
-            </div>
-          </div>
+          <Notification.Root>
+            <Notification.Icon icon={Rocket} />
+            <Notification.Content text="Você recebeu um convite para fazer parte da empresa Rocketseat." />
+            <Notification.Actions>
+              <Notification.Action icon={Check} />
+            </Notification.Actions>
+          </Notification.Root>
         </div>
       </div>
 
@@ -65,39 +62,26 @@ export function Widget() {
         </div>
 
         <div className="divide-y-2 divide-zinc-300 dark:divide-zinc-950">
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-500 dark:text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-          </div>
+          <Notification.Root>
+            <Notification.Icon icon={Rocket} />
+            <Notification.Content text="Você recebeu um convite para fazer parte da empresa Rocketseat." />
+            <Notification.Actions>
+              <Notification.Action onClick={handleClickIgnore} icon={X} />
+            </Notification.Actions>
+          </Notification.Root>
 
-          <div className="bg-zinc-200 dark:bg-zinc-900 px-8 py-4 flex items-start gap-6">
-            <Rocket className="w-6 h-6 text-violet-500 mt-3" />
-            <div className="flex-1 flex flex-col gap-2">
-              <p className="text-sm leading-relaxed text-zinc-600 dark:text-zinc-100">
-                Você recebeu um convite para fazer parte da empresa Rocketseat.
-              </p>
-              <div className="text-xxs text-zinc-400 flex items-center gap-1">
-                <span>Convite</span>
-                <span>Há 3 min</span>
-              </div>
-            </div>
-            <div className="flex gap-2 self-center">
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-zinc-400 hover:bg-zinc-500 dark:bg-zinc-800 dark:hover:bg-zinc-700">
-                <X className="w-3 h-3 text-zinc-50" />
-              </button>
-              <button className="w-8 h-8 rounded flex items-center justify-center bg-violet-500 hover:bg-violet-600">
-                <Check className="w-3 h-3 text-zinc-50" />
-              </button>
-            </div>
-          </div>
+          <Notification.Root>
+            <Notification.Icon icon={Rocket} />
+            <Notification.Content text="Você recebeu um convite para fazer parte da empresa Rocketseat." />
+            <Notification.Actions>
+              <Notification.Action onClick={handleClickIgnore} icon={X} />
+              <Notification.Action
+                onClick={handleClickAccept}
+                icon={Check}
+                className="bg-violet-500 hover:bg-violet-600"
+              />
+            </Notification.Actions>
+          </Notification.Root>
         </div>
       </div>
     </div>
